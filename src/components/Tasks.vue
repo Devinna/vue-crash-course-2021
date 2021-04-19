75175b9888cb52519d08d4f5ee604a5bd605cbb0
Devinnakie <devinnakie@gmail.com>
2021-04-19 15:36:06 +0700

    second commit

---
+++ src/components/Tasks.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task :task="task" />
+    <!-- <h3>{{ task.text }}</h3> -->
+  </div>
+</template>
+
+<script>
+import Task from './Task'
+export default {
+  name: 'Tasks',
+  props: {
+    tasks: Array,
+  },
+  component: { Task },
+}
+</script>
