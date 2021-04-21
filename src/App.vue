2c516b4cba4ba3bf09ab9d47eec7e698d16f331f
Devinnakie <devinnakie@gmail.com>
2021-04-21 16:17:40 +0700

    toggleReminder, AddTask

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" />
-    <Tasks @delete-task="deleteTask" :tasks="tasks" />
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
+    <AddTask v-show="showAddTask" @add-task="addTask" />
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 export default {
   name: 'App',
-  components: { Header, Tasks },
+  components: { Header, Tasks, AddTask },
   data() {
     return {
       tasks: [],
+      showAddTask: false,
     }
   },
   methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
+    addTask(task) {
+      this.tasks = [...this.tasks, task]
+    },
     deleteTask(id) {
       if (confirm('Are you sure?')) {
         this.tasks = this.tasks.filter((task) => task.id != id)
       }
+    },
+    toggleReminder(id) {
+      this.tasks.forEach((task) => {
+        if (task.id === id) {
+          task.reminder = !task.reminder
+        }
+      })
+      //using Array.map
+      // this.tasks = this.tasks.map((task) =>
+      //   task.id === id ? {...task, reminder: !task.reminder } : task
+      // )
     },
   },
   created() {
