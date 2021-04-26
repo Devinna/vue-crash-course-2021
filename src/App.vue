7fceede456ca80d06e4505205efe5c89de7b1441
Devinnakie <devinnakie@gmail.com>
2021-04-26 15:00:27 +0700

    implement router

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,21 @@
       title="Task Tracker"
       :showAddTask="showAddTask"
     />
-    <AddTask v-show="showAddTask" @add-task="addTask" />
-    <Tasks
-      @toggle-reminder="toggleReminder"
-      @delete-task="deleteTask"
-      :tasks="tasks"
-    />
+
+    <router-view :showAddTask="showAddTask"></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+
 export default {
   name: 'App',
-  components: { Header, Tasks, AddTask },
+  components: { Header, Footer },
   data() {
     return {
-      tasks: [],
       showAddTask: false,
     }
   },
@@ -31,64 +27,6 @@ export default {
     toggleAddTask() {
       this.showAddTask = !this.showAddTask
     },
-    async addTask(task) {
-      const res = await fetch('api/tasks', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(task),
-      })
-      const data = await res.json()
-      this.tasks = [...this.tasks, data]
-    },
-    async deleteTask(id) {
-      if (confirm('Are you sure?')) {
-        const res = await fetch(`api/tasks/${id}`, {
-          method: 'DELETE',
-        })
-        res.status === 200
-          ? (this.tasks = this.tasks.filter((task) => task.id != id))
-          : alert('Error deleting')
-      }
-    },
-    async toggleReminder(id) {
-      const taskToToggle = await this.fetchTask(id)
-      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
-
-      const res = await fetch(`api/tasks/${id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(updTask),
-      })
-
-      const data = await res.json()
-
-      // this.tasks.forEach((task) => {
-      //   if (task.id === id) {
-      //     task.reminder = data.reminder
-      //   }
-      // })
-      //using Array.map
-      this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: data.reminder } : task
-      )
-    },
-    async fetchTasks() {
-      const res = await fetch('api/tasks')
-      const data = await res.json()
-      return data
-    },
-    async fetchTask(id) {
-      const res = await fetch(`api/tasks/${id}`)
-      const data = await res.json()
-      return data
-    },
-  },
-  async created() {
-    this.tasks = await this.fetchTasks()
   },
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,10 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
+      v-show="homePage"
       @btn-click="$emit('toggle-add-task')"
-      :text="showAddTask ? 'Close': 'Add Task'"
-      :color="showAddTask? 'red' : 'green'"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
     />
   </header>
 </template>
@@ -15,10 +16,19 @@ export default {
   name: 'Header',
   props: {
     title: String,
-    showAddTask : Boolean
+    showAddTask: Boolean,
   },
   components: {
     Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === '/') {
+        return true
+      } else {
+        return false
+      }
+    },
   },
 }
 </script>
